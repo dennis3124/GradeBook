2ea1b80fe5c24c5b93ea10a952da0cbbf55085c7
ayew <yewaili@gmail.com>
2016-10-01 12:58:43 -0400

    Modified server.js

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-//server.js
-
-//setup
+// Server.js
+// Modules
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+
+// Config
+mongoose.connect('mongodb://localhost/gradebook');
+app.use(express.static(_dirname + "/"));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
 
-app.get('/', function(req, res) {
-	res.send('Hello world!');
-});
+// Define schemas
+// var studentSchema
+// var gradesSchema
+// var coursesSchema
 
-app.listen(3000, function() {
-	console.log('App listening on port 3000');
-});
+// Define API routes
+// General error handling
+function handleError(res, reason, message, code) {
+	console.log("Error: " + reason);
+	res.status(code || 500).json({"error": message});
+}
+

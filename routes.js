a7b54c6a5ee20501d5a2837a76552a62c7ecde1d
Dennis Chia <Dennis Chia>
2016-10-14 16:12:28 -0400

    changed route.js

--- routes.js
+++ routes.js
@@ -4,8 +4,6 @@
 var Student = require('./models/student');
 var Semesters = require('./models/semesters');
 var Courses = require('./models/courses');
-var Section = require('./models/section');
-var Grade = require('./models/section');
 
 module.exports = function(app) {
 	// server routes
@@ -22,6 +20,18 @@ module.exports = function(app) {
 		});
 	});
 
+	app.get('/api/semester/:semesterId', function(req,res) {
+		//get the semester with the semesterID
+		Semesters.find({
+			_id: req.params.semesterId
+		}, function(err,semester){
+			if(err)
+					res.send(err)
+			res.json(semester);
+		});
+	});
+
+	//get all courses with the semesterID
 	app.get('/api/courses/:semesterId',function(req,res){
 		Courses.find({
 			semesterId: req.params.semesterId
@@ -55,10 +65,12 @@ module.exports = function(app) {
 		});
 	});
 
-	app.post('/api/grades', function(req,res) {
+
+	app.post('/api/courses', function(req,res) {
 		Courses.create({
 			courseName: req.body.courseName,
-			semesterId: req.body.semesterId
+			courseId: req.body.courseId,
+			semesterId: req.body.semesterId,
 		},function(err,data) {
 			if (err)
 				res.send(err);
@@ -90,53 +102,6 @@ module.exports = function(app) {
 				res.send(err);
 		});
 
-	});
-
-
-	app.post('/api/section', function(req, res) {
-
-		Section.create({
-			sectionName: req.body.sectionName,
-			courseId: req.body.courseId
-		},function(err,data){
-			if (err)
-				res.send(err);
-		});
-
-	});
-	app.post('/api/grade', function(req, res) {
-
-		Grade.create({
-			grade: req.body.grade,
-			sectionId: req.body.sectionId,
-			totalGrade: req.body.totalGrade
-		},function(err,data){
-			if (err)
-				res.send(err);
-		});
-
-	});
-
-	app.get('/api/grade/:sectionId', function(req,res) {
-		// use mongoose to get current grade with the section ID
-		Grade.find({
-			sectionId: req.params.studentId,
-		},function(err, grades) {
-			if (err) // Error handling
-				res.send(err);
-			res.json(grades); // return all students in JSON
-		});
-	});
-
-	app.get('/api/section/:courseId', function(req,res) {
-		// use mongoose to get sections with course ID
-		Section.find({
-			courseId: req.params.studentId,
-		},function(err, sections) {
-			if (err) // Error handling
-				res.send(err);
-			res.json(sections); // return all students in JSON
-		});
 	});
 
 	// Any routes to handle creating or deleting goes here?

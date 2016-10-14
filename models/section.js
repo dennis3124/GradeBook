d5ea80c2be7dd9e0bdd565ecf53e1227aba8bb04
ayew <yewaili@gmail.com>
2016-10-14 13:58:12 -0400

    Added routes

---
+++ models/section.js
@@ -0,0 +1,12 @@
+// app/models/semesters.js
+// grab mongoose module
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var section = new Schema( {
+	courseId: String,
+	sectionName: String
+})
+// define student model
+// module.export allows us to pass this to other files when it is called.
+module.exports = mongoose.model('Section', section);

7480a7628d527307cb5b5f32552472fd651a9744
Pranav Shekar <Pranav Shekar>
2016-11-02 16:56:42 -0400

    fixed dialog only showing current semester

--- app/components/gpa/dialog/gpaDialogController.js
+++ app/components/gpa/dialog/gpaDialogController.js
@@ -1,21 +1,15 @@
 (function(){
 	angular.module('GradeBook')
-		.controller('gpaDialogController', ['$mdDialog', 'studentService', function($mdDialog, studentService){
+		.controller('gpaDialogController', ['$mdDialog', 'studentService', 'semester', function($mdDialog, studentService, semester){
 			var vm = this;
 			vm.courses = [];
-			vm.semester={};
-			studentService.getCurrentSemester("0027756773").then(function(data) {
-				vm.semester = data.data;
-				vm.semester= vm.semester[0];
-				vm.semesterId = vm.semester._id;
-				console.log(vm.semester);
-			studentService.getCourses(vm.semesterId).then(function(data) {
+			vm.semester= semester;
+			studentService.getCourses(vm.semester._id).then(function(data) {
 				vm.courses = data.data;
-				//console.log(vm.courses);
+				console.log(vm.courses);
 			});
 				//console.log(vm.semesterId);
-			});
-			
+
 		}])
 
 })()
--- app/components/gpa/gpaController.js
+++ app/components/gpa/gpaController.js
@@ -10,12 +10,15 @@
 			});
 
 
-			vm.showAdvanced = function(ev) {
+			vm.showAdvanced = function(ev, semester) {
 			    $mdDialog.show({
 			      controller: "gpaDialogController",
 			      controllerAs: "gpaDialogVM",
 			      templateUrl: 'app/components/gpa/dialog/gpaDialog.html',
 			      parent: angular.element(document.body),
+			      locals: {
+			      		semester: semester
+			      },
 			      targetEvent: ev,
 			      clickOutsideToClose:true,
 			      fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.

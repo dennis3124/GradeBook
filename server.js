4bd5435b267bed02dc85015be0325b2482de2bad
michael Mertz <mertz1@purdue.edu>
2016-11-08 16:39:09 -0500

    Added Logout

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(function (req, res, next) {
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)

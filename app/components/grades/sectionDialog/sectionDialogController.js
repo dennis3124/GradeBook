72ddadb593ee378f4e3775620462ab447b432b63
Avinash Prabhakar <Avinash Prabhakar>
2016-11-10 00:26:58 -0500

    final grade for gradeController.js

--- app/components/grades/sectionDialog/sectionDialogController.js
+++ app/components/grades/sectionDialog/sectionDialogController.js
@@ -3,12 +3,9 @@
 		.controller('sectionDialogController', ['$mdDialog','studentService', 'courseUniqueId',function($mdDialog,studentService,courseUniqueId){
 			var vm = this;
 			vm.section = {name: '', weight: '', courseId: courseUniqueId};
-			vm.flag = 0;
 			vm.submitSection = function(){
 				//console.log(vm.semester);
-				if(vm.flag = 0) {
-					studentService.postSection(vm.section).then(vm.flag = 1);
-				};
+				studentService.postSection(vm.section)
 				$mdDialog.hide();
 			};
 		}])

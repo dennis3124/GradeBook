e018ac2be0bc7782974ad371697e59a86be0e47c
Michael Mertz <michaelmertz013@gmail.com>
2016-12-06 02:21:24 -0500

    added guest users as well as duplicate email error

--- app/components/Login/loginController.js
+++ app/components/Login/loginController.js
@@ -5,8 +5,8 @@
         .module('GradeBook')
         .controller('loginController', LoginController);
 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService', 'UserService', '$state', '$scope'];
-    function LoginController($location, AuthenticationService, FlashService, UserService, $state, $scope) {
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService', 'UserService', '$state', '$scope', '$window'];
+    function LoginController($location, AuthenticationService, FlashService, UserService, $state, $scope, $window) {
         var vm = this;
 
         vm.login = login;
@@ -51,6 +51,55 @@
                     }
             });
         };
+
+        function randomString(length, chars) {
+            var result = '';
+            for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+            return result;
+        };
+
+        vm.useWithoutAccount = function() {
+            vm.dataLoading = true;
+            vm.unknown = {};
+            vm.unknown.id = randomString(12, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+            vm.unknown.name = "Guest";
+
+            do {
+                var duplicate = true;
+            }while (duplicate == 'true') {
+                UserService.GetById(vm.unknown.id)
+                .then(function (response) {
+                    console.log(response);
+                    if (response.data.length != 0) {
+                        vm.duplicateId = true;
+                        console.log(vm.duplicateId);
+                    }
+                    else {
+                        vm.duplicateId = false;
+                        console.log(vm.duplicateId);
+                        vm.unknown.password = randomString(12, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+                        UserService.Create(vm.unknown)
+                                        .then(function (response) {
+                                            if (response.status == 200) {
+                                                FlashService.Success('Registration successful', true);
+                                                $location.path('/login');
+                                            } else {
+                                                FlashService.Error(response.message);
+                                                vm.dataLoading = false;
+                                            }
+                                        });
+                        duplicate = false;
+                        UserService.SetCredentials(vm.unknown.id, vm.unknown.password);
+                        $state.go('login');
+                        $state.reload('login');
+                        $window.location.reload();
+                    }
+                })
+            }
+
+        };
+
+        
 
 
     }
